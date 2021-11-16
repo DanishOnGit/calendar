3152832e301a76df4c7dba80736c978070195f0c
DanishOnGit <youcandoit1911@gmail.com>
2021-11-16 14:29:13 +0530

    feat:current day filter

--- src/App.js
+++ src/App.js
@@ -4,35 +4,68 @@ import { CreateEvent } from "./components/CreateEvent";
 import { EventCard } from "./components/EventCard";
 import { getSortedEvents } from "./utils/getSortedEvents";
 import "react-toastify/dist/ReactToastify.css";
-
+import moment from "moment";
 
 function App() {
   const [events, setEvents] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [filterChecked, setFilterChecked] = useState(false);
   const sortedEvents = getSortedEvents(events);
   const themes = ["green", "blue", "orange"];
 
+  const filteredEventsList = (function filteredEvents() {
+    const result = sortedEvents.filter((event) =>
+      filterChecked
+        ? moment(event.startTime).format("YYYY-MM-DD") ===
+          moment().format("YYYY-MM-DD")
+        : true
+    );
+    // setEvents(result);
+    return result
+  })();   
+
+
   return (
     <div className="App p-4 max-w-xl mx-auto">
-      <h1 className="app-title mb-5 sm:mb-8 text-red-800 text-4xl text-center font-extrabold"> Kalendly </h1>
+      <h1 className="app-title mb-5 sm:mb-8 text-red-800 text-4xl text-center font-extrabold">
+        {" "}
+        Kalendly{" "}
+      </h1>
+      {events.length > 0 && (
+        <div>
+          <input
+            id="today-events"
+            type="checkbox"
+            checked={filterChecked}
+            onChange={()=>setFilterChecked(!filterChecked)}
+          />
+          <label htmlFor="today-events">Show only today's events</label>
+        </div>
+      )}
       <CreateEvent
-      events={events}
+        events={events}
         setEvents={setEvents}
         showModal={showModal}
         setShowModal={setShowModal}
       />
-      <ul>
-        {sortedEvents.map((event, idx) => {
-          return (
-            <EventCard
-            key={event.id}
-              event={event}
-              theme={idx >= 3 ? themes[idx % 3] : themes[idx]}
-            />
-          );
-        })}
-      </ul>
 
+      {events.length === 0 ? (
+        <h1 className="text-gray-400 text-2xl text-center mt-40">
+          No events added yet!
+        </h1>
+      ) : (
+        <ul>
+          {filteredEventsList.map((event, idx) => {
+            return (
+              <EventCard
+                key={event.id}
+                event={event}
+                theme={idx >= 3 ? themes[idx % 3] : themes[idx]}
+              />
+            );
+          })}
+        </ul>
+      )}
       <button
         onClick={() => setShowModal(true)}
         className="z-10 add-event absolute bottom-4 right-4 rounded-lg p-2 text-white"
@@ -52,7 +85,7 @@ function App() {
           ></path>
         </svg>
       </button>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   );
 }
--- src/utils/checkSlotAvailability.js
+++ src/utils/checkSlotAvailability.js
@@ -1,7 +1,7 @@
 
 
 export function checkSlotAvailability(time,events){
-  const result = events.find(event=>event.startTime==time)
+  const result = events.find(event=>event.startTime===time)
   console.log('slot util',result)
   return !!result
 }
